7655f2952b571baf79e12bf8156b769e05dc3474
Ruslan_Amro <ruslan.amrayev85@gmail.com>
2023-08-06 15:02:00 -0400

    framework setup

--- src/test/java/com/library/steps/Hooks.java
+++ src/test/java/com/library/steps/Hooks.java
@@ -8,34 +8,27 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.util.concurrent.TimeUnit;
-
 public class Hooks {
 
     @Before
-    public void setUp(){
+    public void setUp() {
         System.out.println("this is coming from BEFORE");
-        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
-
 
     }
 
     @After
-    public void tearDown(Scenario scenario){
+    public void tearDown(Scenario scenario) {
         System.out.println("this is coming from AFTER");
 
-        if(scenario.isFailed()){
+        if (scenario.isFailed()) {
             final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot,"image/png","screenshot");
+            scenario.attach(screenshot, "image/png", "screenshot");
         }
 
         Driver.closeDriver();
 
     }
-
-
 
 
 }
