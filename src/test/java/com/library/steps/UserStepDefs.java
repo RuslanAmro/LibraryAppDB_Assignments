0662765e66659778c50c0470ec5202600d79515a
Ruslan_Amro <ruslan.amrayev85@gmail.com>
2023-08-09 11:39:23 -0400

    finished us02

--- src/test/java/com/library/steps/UserStepDefs.java
+++ src/test/java/com/library/steps/UserStepDefs.java
@@ -1,5 +1,7 @@
 package com.library.steps;
 
+import com.library.pages.DashBoardPage;
+import com.library.pages.LoginPage;
 import com.library.utility.DB_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,11 +10,19 @@ import org.junit.Assert;
 
 import java.util.List;
 
-public class US01_UserStepDefs {
+public class UserStepDefs {
 
+    //US01 - 1 - 2
     String actualUserCount;
     List<String> actualAllColumns;
 
+    //US02
+    LoginPage loginPage = new LoginPage();
+    DashBoardPage dashBoardPage = new DashBoardPage();
+    String actualBorrowedNumber;
+
+
+    //US01 - 1
     @Given("Establish the database connection")
     public void establish_the_database_connection() {
         //Make a conn with library
@@ -52,6 +62,8 @@ public class US01_UserStepDefs {
         System.out.println("--------------------------------------------------");
     }
 
+
+    //US01 - 2
     @When("Execute query to get all columns")
     public void execute_query_to_get_all_columns() {
 
@@ -68,6 +80,36 @@ public class US01_UserStepDefs {
 
         System.out.println("expectedAllColumns = " + expectedAllColumns);
         Assert.assertEquals(expectedAllColumns, actualAllColumns);
+
+    }
+
+
+    //US02
+    @Given("the {string} on the home page")
+    public void the_on_the_home_page(String librarian) {
+
+        loginPage.login(librarian);
+
+    }
+
+    @When("the librarian gets borrowed books number")
+    public void the_librarian_gets_borrowed_books_number() {
+
+        actualBorrowedNumber = dashBoardPage.borrowedBooksNumber.getText();
+        System.out.println("actualBorrowedNumber = " + actualBorrowedNumber);
+
+    }
+
+    @Then("borrowed books number information must match with DB")
+    public void borrowed_books_number_information_must_match_with_db() {
+
+        String query = "select count(*) from book_borrow where is_returned = 0";
+        DB_Util.runQuery(query);
+
+        String expectedBorrowedNumber = DB_Util.getFirstRowFirstColumn();
+        System.out.println("expectedBorrowedNumber = " + expectedBorrowedNumber);
+
+        Assert.assertEquals(expectedBorrowedNumber, actualBorrowedNumber);
 
     }
 }
