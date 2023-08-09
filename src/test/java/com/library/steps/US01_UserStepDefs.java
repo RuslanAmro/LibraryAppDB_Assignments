714079ff5d41988de1fdb4d75249effcf781b7a4
Ruslan_Amro <ruslan.amrayev85@gmail.com>
2023-08-09 12:07:55 -0400

    finished us02

--- src/test/java/com/library/steps/US01_UserStepDefs.java
+++ src/test/java/com/library/steps/US01_UserStepDefs.java
@@ -10,16 +10,11 @@ import org.junit.Assert;
 
 import java.util.List;
 
-public class UserStepDefs {
+public class US01_UserStepDefs {
 
     //US01 - 1 - 2
     String actualUserCount;
     List<String> actualAllColumns;
-
-    //US02
-    LoginPage loginPage = new LoginPage();
-    DashBoardPage dashBoardPage = new DashBoardPage();
-    String actualBorrowedNumber;
 
 
     //US01 - 1
@@ -72,7 +67,6 @@ public class UserStepDefs {
 
         actualAllColumns = DB_Util.getAllColumnNamesAsList();
         System.out.println("actualAllColumns = " + actualAllColumns);
-
     }
 
     @Then("verify the below columns are listed in result")
@@ -80,36 +74,6 @@ public class UserStepDefs {
 
         System.out.println("expectedAllColumns = " + expectedAllColumns);
         Assert.assertEquals(expectedAllColumns, actualAllColumns);
-
-    }
-
-
-    //US02
-    @Given("the {string} on the home page")
-    public void the_on_the_home_page(String librarian) {
-
-        loginPage.login(librarian);
-
-    }
-
-    @When("the librarian gets borrowed books number")
-    public void the_librarian_gets_borrowed_books_number() {
-
-        actualBorrowedNumber = dashBoardPage.borrowedBooksNumber.getText();
-        System.out.println("actualBorrowedNumber = " + actualBorrowedNumber);
-
-    }
-
-    @Then("borrowed books number information must match with DB")
-    public void borrowed_books_number_information_must_match_with_db() {
-
-        String query = "select count(*) from book_borrow where is_returned = 0";
-        DB_Util.runQuery(query);
-
-        String expectedBorrowedNumber = DB_Util.getFirstRowFirstColumn();
-        System.out.println("expectedBorrowedNumber = " + expectedBorrowedNumber);
-
-        Assert.assertEquals(expectedBorrowedNumber, actualBorrowedNumber);
 
     }
 }
