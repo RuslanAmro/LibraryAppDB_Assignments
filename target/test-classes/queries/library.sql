7447401460f4bfa2e708dfb12b5791dfea6432ad
Ruslan_Amro <ruslan.amrayev85@gmail.com>
2023-08-09 23:00:18 -0400

    finished us04 and us05

--- target/test-classes/queries/library.sql
+++ target/test-classes/queries/library.sql
@@ -1,25 +1,45 @@
+-- US01-1
 select count(id)
 from users; -- actual
 
-select count(distinct id)
-from users; -- expected -- US01_AC01
 
+select count(distinct id)
+from users;
+-- expected -- US01_AC01
+
+-- US01-2
 select *
 from users;
--- US01_AC02
 
 -- ---------------
 
+-- US02
 select count(*)
 from book_borrow
 where is_returned = 0;
--- US02
 
 -- ---------------
 
+-- US03
 select name
-from book_categories; -- US03
+from book_categories;
 
+-- ---------------
+
+-- US04
+select *
+from books
+where name = 'Clean Code';
+
+-- --------------
+
+-- US05
+select bc.name, count(*)
+from book_borrow bb
+         inner join books b on bb.book_id = b.id
+         inner join book_categories bc on b.book_category_id = bc.id
+group by name
+order by 2 desc;
 
 
 
